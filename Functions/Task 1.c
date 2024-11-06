afbf049636447c77be68dcd0520e2d7c058dc212
Mart0GD <marty0545@gmail.com>
2024-11-06 17:52:44 +0200

    Functions

---
+++ Functions/Task 1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/*
+Напишете програма, която прочита от конзолата цяло число и принтира абсолютната стойност на числото. В решението на задачата създайте и използвайте
+функция absolute, която приема цяло число като аргумент и връща цяло чисо.
+*/
+
+int absolute(int number) {
+	return number < 0 ? -number : number;
+}
+
+int main1()
+{
+	int number;
+
+	scanf_s("%d", &number);
+	
+	printf("%d", absolute(number));
+	return 0;
+}
---
+++ Functions/Task 6.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<limits.h>
+
+/*
+Напишете програма, която прочита 2 цели числа start и end и принтира всички числа в интервала [start, end], които имат еднакви съседни цифри (примерни числа: 77, 112, 100). В реализацията на тази програма създайте и използвайте функция предикат hasNeighboringIdenticalDigits.
+*/
+
+int hasNeighboringIdenticalDigits(int number)
+{
+	int currentDigit = INT_MIN; // da sa neshto random prosto da ne se zasekat
+	int previousDigit = INT_MAX;
+
+	while (number != 0)
+	{
+		currentDigit = number % 10;
+		number /= 10;
+
+		if (currentDigit == previousDigit)
+		{
+			return 1;
+		}
+
+		previousDigit = currentDigit;
+	}
+
+	return 0;
+}
+
+int main6()
+{
+	int start, end;
+	scanf_s("%d %d", &start, &end);
+
+	for (int i = start; i <= end; i++)
+	{
+		if (hasNeighboringIdenticalDigits(i))
+		{
+			printf("%d\n", i);
+		}
+	}
+}

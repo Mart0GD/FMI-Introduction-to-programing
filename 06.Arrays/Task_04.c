43757558afab8a2a83c6ee59e8decb9dc74bc507
Mart0GD <122825014+Mart0GD@users.noreply.github.com>
2024-11-26 18:29:01 +0200

    arrays

--- 06.Arrays/Task_04.c
+++ 06.Arrays/Task_04.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 #include<limits.h>
 #define ARR_LENGTH 128
-
+/*
+Напишете програма, която прочита от конзолата цяло неторицателно число n (<= 128), след което прочита n цели числа, запазва ги в масив и после извежда числата в конзолата, които се повтарят.
+*/
 fillArray_04(const arr[], const int arrLength) {
 
 	for (int i = 0; i < arrLength; i++)
@@ -10,7 +12,7 @@ fillArray_04(const arr[], const int arrLength) {
 	}
 }
 
-int isNumberInArray(const int arr[], const int number, const int arrLength) {
+int isNumberInSeenArray(const int arr[], const int number, const int arrLength) {
 
 	for (int i = 0; i < arrLength; i++)
 	{
@@ -23,29 +25,34 @@ int isNumberInArray(const int arr[], const int number, const int arrLength) {
 	return 0;
 }
 
-void findUniqueNumbersInArray(int arr[], const int arrLength)
+void findAndPrintUniqueNumbersInArray(int arr[], const int arrLength)
 {
 	int seenNumbers[ARR_LENGTH] = { 0 };
 
+	int position = 0;
+	int seenNumbersLength = 0;
 	for (int i = 0; i < arrLength; i++)
 	{
-		if (!isNumberInArray(arr, arr[i], arrLength))
+		if (!isNumberInSeenArray(seenNumbers, arr[i], arrLength))
 		{
-			seenNumbers[arr[i]];
+			seenNumbers[position++] = arr[i];
+			seenNumbersLength++;
 		}
 	}
 
+	for (int i = 0; i < seenNumbersLength; i++)
+	{
+		printf("%d. -> %d\n", i + 1, seenNumbers[i]);
+	}
 }
 
-/*
-Напишете програма, която прочита от конзолата цяло неторицателно число n (<= 128), след което прочита n цели числа, запазва ги в масив и после извежда числата в конзолата, които се повтарят.
-*/
-int main()
+int main_04()
 {
 	unsigned short n;
 	int arr[ARR_LENGTH];
 	scanf_s("%hu", &n);
 
 	fillArray_04(arr, n);
-	findUniqueNumbersInArray(arr, n);
+	findAndPrintUniqueNumbersInArray(arr, n); // !functions should only be responsible for one thing!
+	return 0;
 }

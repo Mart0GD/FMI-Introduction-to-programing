2dfbfada8924476e00168309afec7e01cda47af0
Mart0GD <122825014+Mart0GD@users.noreply.github.com>
2024-11-27 20:07:47 +0200

    Loops

--- 04.Loops/Task 11.c
+++ 04.Loops/Task 11.c
@@ -1,28 +1,54 @@
 #include<stdio.h>
 
-int main11() 
+/*
+Напишете програма, която прочита от конзолата цяло неотрицателно число n, след което принтира всички подмножества на дадено множество от n елемента. Като елементите ще са числата от 1 до n.
+
+ЗАБЕЛЕЖКА: Изходните данни не е задължително да са в същият ред като долупоказаните.
+#1 Пример
+Вход:
+3
+
+Изход:
+{ }
+{ 1 }
+{ 2 }
+{ 1 2 }
+{ 3 }
+{ 1 3 }
+{ 2 3 }
+{ 1 2 3 }
+*/
+int main()
 {
 	int n;
 	scanf_s("%d", &n);
 
+	int step = 1;
 	puts("{ }");
-	if (n == 0) { return; }
-
-	int combinations = 0;
-	int combinationsTemp;;
-	for (int i = 1; i <= n; i++)
+	for (int begining = 0; begining <= n; begining++)
 	{
-		combinationsTemp = 1;
-		printf("{ %d }\n", i);
-		for (int j = 1; j < i; j++)
+		//0
+		step = 1;
+		for (int currentFirstNumber = 0; currentFirstNumber < begining; currentFirstNumber++)
 		{
-			printf("{ %d %d }\n", j, i);
-			combinationsTemp++;
+			//0
+			printf("{ ");
+			for (int currentNumber = currentFirstNumber + 1; currentNumber <= begining; currentNumber += step)
+			{
+				printf("%d ", currentNumber);
+				/*for (int i = currentNumber; currentNumber < begining; currentNumber++)
+				{
+					printf("%d ", currentNumber);
+				}*/
+
+			}
+			puts("}");
+			step++;
+			
+			
 		}
-
-
-		combinations+= combinationsTemp;
 	}
-
+	
+	
 	return 0;
 }

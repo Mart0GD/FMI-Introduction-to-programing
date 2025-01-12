a554b56533637805930cce1285f4287b8ff7a3fd
Mart0GD <122825014+Mart0GD@users.noreply.github.com>
2025-01-12 14:14:08 +0200

    Homework three

--- Homework/Homework Three/4MI0400362_2_02.c
+++ Homework/Homework Three/4MI0400362_2_02.c
@@ -1,399 +1,321 @@
-//#include<stdio.h>
-//#include<stdlib.h>
-//#include "Functions.h"
-//
-//#pragma warning(push)
-//#pragma warning(disable: 4996)
-//
-//#define WORDS_BUFFER 64
-//#define MEMORY_MESSAGE "No available memory to allocate"
-//
-//void freeCharMatrix(char** matrix, const unsigned size) {
-//	for (int i = 0; i < size; i++)
-//		free(matrix[i]);
-//	free(matrix);
-//}
-//
-//char** createCharMatrix(const unsigned rows) {
-//
-//	char** matrix = (char**)malloc(sizeof(char*) * rows);
-//	if (!matrix) {
-//		puts(MEMORY_MESSAGE);
-//		exit(EXIT_FAILURE);
-//	}
-//
-//	for (int i = 0; i < rows; i++) {
-//		matrix[i] = (char*)malloc(sizeof(char) * WORDS_BUFFER);
-//		if (!matrix[i]) {
-//			puts(MEMORY_MESSAGE);
-//			freeCharMatrix(matrix, i);
-//			exit(EXIT_FAILURE);
-//		}
-//	}
-//
-//	return matrix;
-//}
-//
-//char** copyMatrix(const char** matrix, const unsigned size) {
-//	char** copy = createCharMatrix(size);
-//
-//	for (int i = 0; i < size; i++){
-//		copy[i] = substr(matrix[i], 0, WORDS_BUFFER);
-//	}
-//
-//	return copy;
-//}
-//
-//void printWords(char** matrix, int size) {
-//	for (int i = 0; i < size; i++){
-//		printf("%s ", matrix[i]);
-//	}
-//}
-//
-//char* toLower(const char* str) {
-//	const unsigned distance = 'a' - 'A';
-//	int len = strlen(str);
-//
-//	int iter = 0;
-//	char* lower = (char*)malloc(sizeof(char) * (len + 1));
-//	while (*str != '\0') {
-//		if (isUpperWordChar(*str)) { lower[iter] = (*str) + distance; }
-//		else { lower[iter] = *str; }
-//
-//		str++;iter++;
-//	}
-//	lower[iter] = '\0';
-//
-//	return lower;
-//}
-//
-//int isUpperWordChar(const char chr) {
-//	return chr >= 'A' && chr <= 'Z';
-//}
-//
-//int IsDigit(const char chr) {
-//	return
-//		chr >= 'a' && chr <= 'z' ||
-//		chr >= '0' && chr <= '9' ||
-//		chr == '-';
-//}
-//
-//int isNumber(const char chr) {
-//	return chr >= '0' && chr <= '9';
-//}
-//
-//char** split(const char* str, int* count) {
-//	char** words = createCharMatrix(strlen(str), WORDS_BUFFER);
-//
-//	int iter = 0;
-//	int startIndex = 0;
-//	int endIndex = 0;
-//	int wordStarted = 0;
-//	while (*str != '\0'){
-//		if (!wordStarted && IsDigit(*str)){
-//			wordStarted = 1;
-//			startIndex = iter;
-//		}
-//		else if (wordStarted && !IsDigit(*str)) {
-//			wordStarted = 0;
-//			endIndex = iter;
-//
-//			words[(*count)++] = substr(str - iter, startIndex, endIndex);
-//		}
-//
-//		iter++;str++;
-//	}
-//	if (wordStarted){ // one word sentence or no spaces or other splitter chars
-//		words[(*count)++] = substr(str - iter, startIndex, iter);
-//	}
-//
-//	words = (char**)realloc(words, sizeof(char*) * iter);
-//	if (!words){
-//		puts(MEMORY_MESSAGE);
-//		exit(EXIT_FAILURE);
-//	}
-//
-//	return words;
-//}
-//
-//int isSeen(const char* word, const char** uniqueWords, const unsigned count) {
-//	for (int i = 0; i < count; i++)
-//		if (strcmp(word, uniqueWords[i]) == 0) return 1;
-//
-//	return 0;
-//}
-//
-//char** getUniqueWords(const char** words, unsigned* wordsCount, unsigned const allWordsCount) {
-//	char** uniqueWords = createCharMatrix(sizeof(char) * allWordsCount);
-//
-//	for (int i = 0; i < allWordsCount; i++){
-//		if (!isSeen(words[i], uniqueWords, *wordsCount)){
-//			uniqueWords[(*wordsCount)++] = substr(words[i], 0, WORDS_BUFFER);
-//		}
-//	}
-//
-//	uniqueWords = (char**)realloc(uniqueWords ,sizeof(char*) * (*wordsCount));
-//	if (!uniqueWords) {
-//		puts(MEMORY_MESSAGE);
-//		exit(EXIT_FAILURE);
-//	}
-//	return uniqueWords;
-//}
-//
-//int* getWordOccurances(const char** uniqueWords, const char** allWords, const unsigned allWordsCount, const unsigned uniqueWordsCount) {
-//
-//	int* wordOccurances = (int*)calloc(uniqueWordsCount, sizeof(int));
-//	if (!wordOccurances) {
-//		puts(MEMORY_MESSAGE);
-//		exit(EXIT_FAILURE);
-//	}
-//
-//	for (int i = 0; i < allWordsCount; i++){
-//		for (int j = 0; j < uniqueWordsCount; j++){
-//			if (strcmp(uniqueWords[j], allWords[i]) == 0){
-//				wordOccurances[j]++;
-//				break;
-//			}
-//		}
-//	}
-//
-//	return wordOccurances;
-//}
-//
-//void sortWords(char** uniqueWords, int* wordsOccurances, const unsigned uniqueWordsCount) {
-//
-//	int swapped = 0;
-//	int temp = 0;
-//	char* temp2 = NULL;
-//	for (int i = 0; i < uniqueWordsCount - 1; i++){
-//		swapped = 0;
-//		for (int j = 0; j < uniqueWordsCount - i - 1; j++){
-//			if (wordsOccurances[j] > wordsOccurances[j + 1]) continue;
-//			else if (wordsOccurances[j] == wordsOccurances[j + 1] && strcmp(uniqueWords[j], uniqueWords[j + 1]) < 0) continue;
-//
-//			temp = wordsOccurances[j];
-//			wordsOccurances[j] = wordsOccurances[j + 1];
-//			wordsOccurances[j + 1] = temp;
-//
-//			temp2 = uniqueWords[j];
-//			uniqueWords[j] = uniqueWords[j + 1];
-//			uniqueWords[j + 1] = temp2;
-//
-//			swapped = 1;
-//		}
-//		if (!swapped) break;
-//	}
-//}
-//
-//int replace(char** text, const unsigned whatStartIndex, const unsigned whatEndIndex, const char* with) {
-//	int textLen = strlen(*text);
-//	int whatLen = whatEndIndex - whatStartIndex;
-//	int withLen = strlen(with);
-//
-//	int diff = whatLen - withLen < 0 ? -(whatLen - withLen) : whatLen - withLen;
-//	if (whatLen > withLen){
-//		int newLen = textLen - diff;
-//
-//		for (int i = whatStartIndex + withLen; i < textLen; i++){
-//			(*text)[i] = (*text)[i + diff];
-//		}
-//
-//		*text = (char*)realloc(*text ,sizeof(char) * (newLen + 1));
-//		if (!(*text)) {
-//			puts(MEMORY_MESSAGE);
-//			exit(EXIT_FAILURE);
-//		}
-//		(*text)[newLen] = '\0';
-//
-//		int iter = whatStartIndex;
-//		while (iter < whatStartIndex + withLen){
-//			(*text)[iter++] = *(with++);
-//		}
-//		return -diff;
-//	}
-//	else if (whatLen < withLen) {
-//		int newLen = textLen + diff;
-//
-//		*text = (char*)realloc(*text, sizeof(char) * (newLen + 1));
-//		if (!(*text)) {
-//			puts(MEMORY_MESSAGE);
-//			exit(EXIT_FAILURE);
-//		}
-//
-//		for (int i = newLen; i > whatStartIndex; i--){
-//			(*text)[i] = (*text)[i - diff];
-//		}
-//		(*text)[newLen] = '\0';
-//
-//		int iter = whatStartIndex;
-//		while (iter < whatStartIndex + withLen) {
-//			(*text)[iter++] = *(with++);
-//		}
-//		return diff;
-//	}
-//	else {
-//		int iter = whatStartIndex;
-//		while (iter < whatStartIndex + withLen){
-//			(*text)[iter++] = *(with++);
-//		}
-//	}
-//
-//	return 0;
-//}
-//
-//char* createCompressedString(const char* text, const char** wordsList, const unsigned uniqueWordsCount) {
-//	char* compressed = substr(text, 0, strlen(text));
-//	if (!compressed) {
-//		puts(MEMORY_MESSAGE);
-//		exit(EXIT_FAILURE);
-//	}
-//
-//	int startIndex = 0;
-//	int endIndex = 0;
-//	int wordStarted = 0;
-//	int iter = 0;
-//
-//	char* what = NULL;
-//	char* with = NULL;
-//	while (compressed[iter] != '\0'){
-//
-//		if (!wordStarted && IsDigit(compressed[iter])) {
-//			startIndex = iter;
-//			wordStarted = 1;
-//		}
-//		else if (wordStarted && !IsDigit(compressed[iter])) {
-//			endIndex = iter;
-//			wordStarted = 0;
-//
-//			what = substr(compressed, startIndex, endIndex);
-//			for (int i = 0; i < uniqueWordsCount; i++){
-//				if (strcmp(what, wordsList[i]) == 0) {
-//					with = toString(i);
-//					break;
-//				}
-//			}
-//
-//			iter += replace(&compressed, startIndex, endIndex, with);
-//		}
-//
-//		iter++;
-//	}
-//	if (wordStarted)
-//	{
-//		what = substr(compressed, startIndex, iter);
-//		for (int i = 0; i < uniqueWordsCount; i++) {
-//			if (strcmp(what, wordsList[i]) == 0) {
-//				with = toString(i);
-//				break;
-//			}
-//		}
-//
-//		iter += replace(&compressed, startIndex, iter, with);
-//	}
-//	compressed[iter] = '\0';
-//
-//	return compressed;
-//}
-//
-//char* createDecompressedString(const char* compressedStr, const char** wordsList, const unsigned wordsListLength) {
-//	char* decompressed = substr(compressedStr, 0, strlen(compressedStr));
-//	
-//	int startIndex = 0;
-//	int endIndex = 0;
-//	int numberStarted = 0;
-//	int iter = 0;
-//
-//	int what = 0;
-//	char* with = NULL;
-//	while (decompressed[iter] != '\0') {
-//
-//		if (!numberStarted && isNumber(decompressed[iter])) {
-//			startIndex = iter;
-//			numberStarted = 1;
-//		}
-//		else if (numberStarted && !isNumber(decompressed[iter])) {
-//			endIndex = iter;
-//			numberStarted = 0;
-//
-//			what = posAtoi(substr(decompressed, startIndex, endIndex));
-//			for (int i = 0; i < wordsListLength; i++) {
-//				if (what == i) {
-//					with = wordsList[i];
-//					break;
-//				}
-//			}
-//
-//			iter += replace(&decompressed, startIndex, endIndex, with);
-//		}
-//
-//		iter++;
-//	}
-//	if (numberStarted)
-//	{
-//		what = posAtoi(substr(decompressed, startIndex, iter));
-//		for (int i = 0; i < wordsListLength; i++) {
-//			if (what == i) {
-//				with = wordsList[i];
-//				break;
-//			}
-//		}
-//
-//		iter += replace(&decompressed, startIndex, iter, with);
-//	}
-//	decompressed[iter] = '\0';
-//
-//	return decompressed;
-//}
-//
-//char* compress(const char* str, char*** wordsList, unsigned* wordsListLength, unsigned* wordsCount) {
-//	char* lowered = toLower(str);
-//
-//	int allWordsCount = 0;
-//	char** words = split(lowered, &allWordsCount);
-//
-//	int uniqueWordsCount = 0;
-//	char** uniqueWords = getUniqueWords(words, &uniqueWordsCount, allWordsCount);
-//	int* wordsOccurances = getWordOccurances(uniqueWords, words, allWordsCount, uniqueWordsCount);
-//
-//	sortWords(uniqueWords, wordsOccurances, uniqueWordsCount);
-//
-//	char* compressedString = createCompressedString(lowered, uniqueWords, uniqueWordsCount);
-//
-//	*wordsList = copyMatrix(uniqueWords, uniqueWordsCount);
-//	*wordsListLength = uniqueWordsCount;
-//	*wordsCount = allWordsCount;
-//
-//	free(lowered);
-//	freeCharMatrix(uniqueWords, uniqueWordsCount);
-//	freeCharMatrix(words, allWordsCount);
-//	free(wordsOccurances);
-//	return compressedString;
-//}
-//
-//int main() {
-//
-//	char* str = readText();
-//
-//	unsigned wordsListLength = 0;
-//	unsigned wordsCount = 0;
-//
-//	char** wordsList = createCharMatrix(strlen(str), WORDS_BUFFER);
-//	char* compressedStr = compress(str, &wordsList, &wordsListLength, &wordsCount);
-//	printf("%s", compressedStr);
-//
-//	puts("\nWords: ");
-//	printWords(wordsList, wordsListLength);
-//	puts(" ");
-//
-//	puts("Decompressed text:");
-//	char* decompressed = createDecompressedString(compressedStr, wordsList, wordsListLength);
-//	printf("%s\n", decompressed);
-//
-//	printf("Are both text equal --> %d", strcmp(str, decompressed));
-//
-//	free(str);
-//	freeCharMatrix(wordsList, wordsListLength);
-//	free(decompressed);
-//	free(compressedStr);
-//	return 0;
-//}
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+#define MAX_TEXT_SIZE 1024
+#define LETTERS_BUFFER 64
+
+#pragma warning(push)
+#pragma warning(disable: 4996)
+
+void reallocate(char** arr, const int newSize) {
+	char* temp = (char*)realloc(*arr, sizeof(char) * (newSize + 1));
+	if (!temp) {
+		puts("reallocation failed!");
+		return;
+	}
+
+	*arr = temp;
+}
+
+char* readText() {
+
+	char* text = (char*)malloc(sizeof(char) * MAX_TEXT_SIZE);
+	if (!text) return text;
+
+	int symb = 0;
+	int iter = 0;
+
+	while ((symb = getc(stdin)) != EOF && symb != '\n') {
+		if (iter < MAX_TEXT_SIZE) text[iter++] = symb;
+		else {
+			puts("Invalid input, no more than 1023 sumbols can be entered!");
+			exit(-1);
+		}
+	}
+
+	reallocate(&text, iter);
+	text[iter] = '\0';
+
+	return text;
+}
+
+void freeCharMatrix(char** matrix, const int size) {
+	for (int i = 0; i < size; i++) {
+		free(matrix[i]);
+	}
+	free(matrix);
+}
+
+int myStricmp(const char* str1, const char* str2) {
+	if (!str1 || !str2) {
+		puts("Null pointer detected!");
+		return -1;
+	}
+	const int diff = 'a' - 'A';
+
+	char chr1;
+	char chr2;
+	while (*str1 && *str2)
+	{
+		chr1 = *str1 >= 'A' && *str1 <= 'Z' ? *str1 + diff : *str1;
+		chr2 = *str2 >= 'A' && *str2 <= 'Z' ? *str2 + diff : *str2;
+
+		if (chr1 != chr2) break;
+		str1++;str2++;
+	}
+	chr1 = *str1 >= 'A' && *str1 <= 'Z' ? *str1 + diff : *str1;
+	chr2 = *str2 >= 'A' && *str2 <= 'Z' ? *str2 + diff : *str2;
+
+	return (int)(chr1 - chr2);
+}
+
+int isValidChar(const char chr) {
+	return
+		chr >= 'A' && chr <= 'Z' ||
+		chr >= 'a' && chr <= 'z' ||
+		chr >= '0' && chr <= '9' ||
+		chr == '-';
+}
+
+char* toString(int number) {
+	int power = 0;
+	int temp = number;
+	while (temp /= 10)
+		power++;
+
+	char* str = (char*)malloc(sizeof(char) * (power + 1));
+	if (!str) return str;
+
+	for (int i = power; i >= 0; i--) {
+		str[i] = (number % 10) + '0';
+		number /= 10;
+	}
+	str[power + 1] = '\0';
+
+	return str;
+}
+
+char** createCharMatrix(const int rows, const int cols) {
+	char** matrix = (char**)malloc(sizeof(char*) * rows);
+	if (!matrix) return matrix;
+
+	for (int i = 0; i < rows; i++) {
+		matrix[i] = (char*)malloc(sizeof(char) * cols);
+		if (!matrix[i]) {
+			puts("No memory to allocate!");
+			freeCharMatrix(matrix, i);
+			exit(-1);
+		}
+	}
+
+	return matrix;
+}
+
+void getNextWord(char** text) {
+	while (*(*text) && !isValidChar(*(*text)))
+		(*text)++;
+}
+
+char* extractWord(char** text) {
+	char* word = (char*)malloc(sizeof(char) * LETTERS_BUFFER);
+	if (!word) return word;
+
+	int wordSize = 0;
+	while (*(*text) && isValidChar(*(*text))) {
+		if (wordSize == LETTERS_BUFFER - 1) {
+			puts("Invalid word detected. Words with more than 64 sybols are not permited!");
+			exit(-1);
+		}
+
+		word[wordSize++] = *(*text);
+		(*text)++;
+	}
+
+	reallocate(&word, wordSize);
+	word[wordSize] = '\0';
+
+	return word;
+}
+
+int isSeen(const char** matrix, const char* word, const int matrixSize) {
+	for (int i = 0; i < matrixSize; i++)
+		if (myStricmp(word, matrix[i]) == 0) return 1;
+
+	return 0;
+
+}
+
+char* toLower(const char* text) {
+	const int diff = 'a' - 'A';
+
+	char* toLower = (char*)malloc(sizeof(char) * (strlen(text) + 1));
+	if (!toLower) return NULL;
+
+	int index = 0;
+	while (*text) {
+		if (*text >= 'A' && *text <= 'Z') toLower[index++] = *text + diff;
+		else toLower[index++] = *text;
+		text++;
+	}
+	toLower[index] = '\0';
+
+	return toLower;
+}
+
+char** getUniqueWords(const char* text, int* uniqueWordsCount) {
+	char** uniqueWords = createCharMatrix(MAX_TEXT_SIZE, LETTERS_BUFFER);
+
+	char* word = NULL;
+	while ((getNextWord(&text), *text)) {
+		word = extractWord(&text);
+
+		if (!isSeen(uniqueWords, word, *uniqueWordsCount)) {
+			uniqueWords[(*uniqueWordsCount)++] = toLower(word);
+		}
+	}
+
+	char** temp = (char**)realloc(uniqueWords, sizeof(char*) * (*uniqueWordsCount));
+	if (!temp) return uniqueWords;
+	uniqueWords = temp;
+
+	return uniqueWords;
+}
+
+char* findMatch(const char* text, const char* what) {
+	char* word = NULL;
+	while ((getNextWord(&text), *text)) {
+		word = extractWord(&text);
+		if (myStricmp(word, what) == 0) return text - strlen(word);
+	}
+
+	return NULL;
+}
+
+int* getWordsOccurances(const char* text, const char** uniqueWords, const int uniqueWordsCount) {
+	int* wordsOccurances = (int*)calloc(uniqueWordsCount, sizeof(int));
+	if (!wordsOccurances) return wordsOccurances;
+
+	char* word = NULL;
+	char* tempTextPtr;
+	for (int i = 0; i < uniqueWordsCount; i++) {
+		tempTextPtr = text;
+		word = findMatch(text, uniqueWords[i]);
+
+		while (word) {
+			wordsOccurances[i]++;
+			word = findMatch(word + strlen(uniqueWords[i]), uniqueWords[i]);
+		}
+	}
+
+	return wordsOccurances;
+}
+
+void sortWords(char** words, int* uniqueWordsOccurances, const int uniqueWordsCount) {
+	int swapped = 0;
+	int temp = 0;
+	char* tempPtr = NULL;
+
+	for (int i = 0; i < uniqueWordsCount - 1; i++) {
+		swapped = 0;
+		for (int j = 0; j < uniqueWordsCount - 1 - i; j++) {
+			if (uniqueWordsOccurances[j] > uniqueWordsOccurances[j + 1]) continue;
+			else if (uniqueWordsOccurances[j] == uniqueWordsOccurances[j + 1] && myStricmp(words[j], words[j + 1]) < 0) continue;
+
+			temp = uniqueWordsOccurances[j];
+			uniqueWordsOccurances[j] = uniqueWordsOccurances[j + 1];
+			uniqueWordsOccurances[j + 1] = temp;
+
+			tempPtr = words[j];
+			words[j] = words[j + 1];
+			words[j + 1] = tempPtr;
+
+			swapped = 1;
+		}
+		if (!swapped) break;
+	}
+}
+
+char* compressText(const char* text, char*** uniqueWords, int* uniqueWordsCount) {
+	*uniqueWords = getUniqueWords(text, uniqueWordsCount);
+
+	int* uniqueWordsOccurances = getWordsOccurances(text, *uniqueWords, *uniqueWordsCount);
+	sortWords(*uniqueWords, uniqueWordsOccurances, *uniqueWordsCount);
+
+	char* compressedText = (char*)malloc(sizeof(char) * strlen(text));
+	if (!compressedText) return compressedText;
+
+	char* word = NULL;
+	char* replacer = NULL;
+	int len = 0;
+	while (*text){
+		
+		if (!isValidChar(*text)) compressedText[len++] = *(text++);
+		else {
+			word = extractWord(&text);
+			for (int i = 0; i < *uniqueWordsCount; i++){
+				if (myStricmp((*uniqueWords)[i], word) == 0){
+					replacer = toString(i);
+
+					strcpy(compressedText + len, replacer);
+					len += strlen(toString(i));
+					break;
+				}
+			}
+		}
+
+	}
+
+	reallocate(&compressedText, len);
+	compressedText[len] = '\0';
+
+	free(uniqueWordsOccurances);
+	return compressedText;
+}
+
+char* decompressText(const char* text, const char** compressedWords, const int compressedWordsCount) {
+	char* decompressed = (char*)malloc(sizeof(char) * MAX_TEXT_SIZE);
+	if (!decompressed) return decompressed;
+
+	int len = 0;
+	int index = 0;
+	while (*text) {
+		if (!isValidChar(*text)) decompressed[len++] = *(text++);
+		else {
+			index = atoi(extractWord(&text));
+
+			strcpy(decompressed + len, compressedWords[index]);
+			len += strlen(compressedWords[index]);
+		}
+	}
+
+	reallocate(&decompressed, len);
+	decompressed[len] = '\0';
+	return decompressed;
+}
+
+int main() {
+	printf("Enter text: ");
+	char* text = readText();
+
+	int uniqueWordsCount = 0;
+	char** uniqueWords = createCharMatrix(MAX_TEXT_SIZE, LETTERS_BUFFER);
+	char* compressed = compressText(text, &uniqueWords, &uniqueWordsCount);
+
+	printf("Compressed text --> %s\n", compressed);
+	printf("Unique words list: \n");
+	for (int i = 0; i < uniqueWordsCount; i++) {
+		printf("%d. %s\n", i, uniqueWords[i]);
+	}
+
+	char* decompressed = decompressText(compressed, uniqueWords, uniqueWordsCount);
+
+	printf("\ndecompressed text --> %s\n", decompressed);
+	printf("\nAre input text and decompressed equal --> %s", myStricmp(text, decompressed) == 0 ? "yes" : "no");
+
+	free(text);
+	free(compressed);
+	free(decompressed);
+	freeCharMatrix(uniqueWords, uniqueWordsCount);
+	return 0;
+}
